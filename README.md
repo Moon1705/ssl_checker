eb99ded2deca4449710abf108ca72fbfb70f2f35
root <root@localhost.localdomain>
2019-07-06 12:56:29 -0500

    Add path to Windows and Linux

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ssl_checker
 SSL Checker is designed to evaluate the work SSL-certificates.
+
+pip3 install PrettyTable
--- ssl_checker_script.py
+++ ssl_checker_script.py
@@ -1,21 +1,26 @@
 import ssllabs_scanner
 import datetime
 import time
+import os
 from prettytable import PrettyTable
+
+def path_str(absol_path):
+	return os.path.abspath(absol_path)
 
 # Open file ../domain/list_of_domain.txt
 def ssl_checker_open_domain_list():
-	with open('domain\\list_of_domain.txt') as f_read:
+	with open(path_str('domain/list_of_domain.txt')) as f_read:
 		list_domains = f_read.read().splitlines()
 	return list_domains
 
 #Write result check, start and end time in ../result/result_dd.mm.YYYY.txt
 def ssl_checker_print_result(table_checks):
-	with open('result\\result_check_' + str(date_check_start.strftime("%d.%m.%Y")) + '.txt','tw') as f_write:
-		f_write.write("Start check: " + str(date_check_start.strftime("%d-%m-%Y %H:%M")) + "\n")
+	data_format = "%d-%m-%Y %H:%M"
+	with open(path_str('result/result.txt'),'tw') as f_write:
+		f_write.write("Start check: " + str(date_check_start.strftime(data_format)) + "\n")
 		f_write.write(table_checks.get_string())
 		date_check_end = datetime.datetime.now()
-		f_write.write("\nEnd check: " + str(date_check_end.strftime("%d-%m-%Y %H:%M")))
+		f_write.write("\nEnd check: " + str(date_check_end.strftime(data_format)))
 
 table_checks = PrettyTable(["Domain", "Status message", "Grade"])
 list_domains = ssl_checker_open_domain_list()
