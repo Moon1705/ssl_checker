4f2b51be0d6cc21931fbb5d5e2b9d9f2e943e231
Moon1705 <t.k.n-24@mail.ru>
2019-07-16 11:18:19 +0300

    Create new main script

---
+++ main.py
@@ -0,0 +1,10 @@
+import datetime
+from moduls import ssl_checker as sc
+from prettytable import PrettyTable
+
+date_check_start = datetime.datetime.now()
+table_checks = PrettyTable(["Domain", "Status message", "Grade"])
+list_domains = sc.open_domain_list()
+table_check_domain = sc.check_domains(list_domains, table_checks)
+sc.print_result(table_check_domain, date_check_start)
+
