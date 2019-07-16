9da49a3d0bdcee91a216b1d3a94d957581c5d421
Moon1705 <t.k.n-24@mail.ru>
2019-07-16 11:15:34 +0300

    Start new main script

--- start_ssl_checker.sh
+++ start_ssl_checker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # If you want to use cron, you need to write the full path to ssl_checker folder in $SSL_CHECKER_PATH_FOLDER.
 SSL_CHECKER_PATH_FOLDER=$(pwd)
-SSL_CHECKER_FILE_NAME=ssl_checker_script.py
+SSL_CHECKER_FILE_NAME=main.py
 SSL_CHECKER_LOG_FILE=log/ssl_checker.log
 cd $SSL_CHECKER_PATH_FOLDER
 # If you want to use python 2.7, you need to write the "python $SSL_CHECKER_FILE_NAME > $SSL_CHECKER_LOG_FILE 2>&1" in next line.
